575eee454bf5b560005d2c47dcf2a070249e26f1
ViktoriiaKharchenko <ivanovad86@gmail.com>
2020-09-23 00:54:52 +0300

    models,startup context files,

---
+++ ApiReactProject/Models/InsuranceContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ApiReactProject
+{
+    public class InsuranceContext : DbContext
+    {
+        public InsuranceContext() { }
+
+
+        public InsuranceContext(DbContextOptions<InsuranceContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+
+            //Database.Migrate();
+        }
+        public DbSet<Brokers> Brokers { get; set; }
+        public DbSet<Categories> Categories { get; set; }
+        public DbSet<BrokersCategories> BrokersCategories { get; set; }
+        public DbSet<Types> Types { get; set; }
+        public DbSet<Clients> Clients { get; set; }
+        public DbSet<Documents> Documents { get; set; }
+
+    }
+}
